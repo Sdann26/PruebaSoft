c6650ef6e289e530f1e345ac71d7fc620419c43d
Danilo Blas <sdann_26@hotmail.com>
2025-09-07 22:30:03 -0500

    chore: create initial configuration

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: { react: pluginReact },
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+]);
